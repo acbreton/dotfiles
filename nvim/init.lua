f13a4e9c418b00ee71ede1c74e8d9b0c23c0c3b4
Alec Breton <acbreton@github.com>
2025-08-26 16:52:03 -0400

    stable to main

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,7 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
+    "--branch=main", lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
