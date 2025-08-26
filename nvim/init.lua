f1eac30e256e48d67341ebc9f98a93a01d1bd212
Alec Breton <acbreton@github.com>
2025-08-26 15:25:06 -0400

    add hardmore to keys

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,22 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local hardmode = true
+
+if hardmode then
+  local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
+
+  vim.api.nvim_set_keymap('i', '<Up>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Down>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Left>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Right>', '<C-o>' .. msg, { noremap = true, silent = false })
+
+  vim.api.nvim_set_keymap('n', '<Up>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Down>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Left>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Right>', msg, { noremap = true, silent = false })
+end
+
 -- Load config modules
 require("settings")
 require("keymaps")
