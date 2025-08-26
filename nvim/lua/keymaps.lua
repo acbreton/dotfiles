da2ebf1db26e9a28b89e3d6cf176522e2cb3a5b2
Alec Breton <acbreton@github.com>
2025-08-26 15:28:48 -0400

    keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -5,3 +5,18 @@ vim.api.nvim_set_keymap("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]]
 -- Show diagnostics with <leader>e
 vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
 
+local hardmode = true
+
+if hardmode then
+  local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
+
+  vim.api.nvim_set_keymap('i', '<Up>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Down>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Left>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Right>', '<C-o>' .. msg, { noremap = true, silent = false })
+
+  vim.api.nvim_set_keymap('n', '<Up>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Down>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Left>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Right>', msg, { noremap = true, silent = false })
+end
