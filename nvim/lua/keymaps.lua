969b02768ac987d6d28d822692a211b2924af37a
Alec Breton <acbreton@github.com>
2025-08-27 11:11:08 -0400

    Refactor key mappings to use vim.keymap.set

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,22 +1,35 @@
 -- Completion keymaps (assuming nvim-cmp is loaded)
-vim.api.nvim_set_keymap("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true, noremap = true })
-vim.api.nvim_set_keymap("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true, noremap = true })
+vim.keymap.set("i", "<Tab>", function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-n>"
+  else
+    return "<Tab>"
+  end
+end, { expr = true, noremap = true })
+
+vim.keymap.set("i", "<S-Tab>", function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-p>"
+  else
+    return "<S-Tab>"
+  end
+end, { expr = true, noremap = true })
 
 -- Show diagnostics with <leader>e
-vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
 
 local hardmode = true
 
 if hardmode then
   local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
 
-  vim.api.nvim_set_keymap('i', '<Up>', '<C-o>' .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('i', '<Down>', '<C-o>' .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('i', '<Left>', '<C-o>' .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('i', '<Right>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.keymap.set('i', '<Up>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.keymap.set('i', '<Down>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.keymap.set('i', '<Left>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.keymap.set('i', '<Right>', '<C-o>' .. msg, { noremap = true, silent = false })
 
-  vim.api.nvim_set_keymap('n', '<Up>', msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Down>', msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Left>', msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Right>', msg, { noremap = true, silent = false })
+  vim.keymap.set('n', '<Up>', msg, { noremap = true, silent = false })
+  vim.keymap.set('n', '<Down>', msg, { noremap = true, silent = false })
+  vim.keymap.set('n', '<Left>', msg, { noremap = true, silent = false })
+  vim.keymap.set('n', '<Right>', msg, { noremap = true, silent = false })
 end
