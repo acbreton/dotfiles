72dbe2c93f538f6bc253da91ea6d2e14490fd889
Alec Breton <acbreton@github.com>
2025-08-27 21:35:42 -0400

    update plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,99 +1,104 @@
 return require("lazy").setup({
-  { "tpope/vim-sensible" },
-  {
-    "NLKNguyen/papercolor-theme",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme PaperColor")
-    end
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
+    { "tpope/vim-sensible" },
+    {
+        "NLKNguyen/papercolor-theme",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme PaperColor")
+        end
     },
-    config = function()
-      require("neo-tree").setup({
-        filesystem = {
-          follow_current_file = true,
-          hijack_netrw_behavior = "open_default",
-          filtered_items = { visible = false },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
         },
-        window = {
-          mappings = {
-            ["<space>"] = "none",
-            ["o"] = "open",
-            ["<cr>"] = "open",
-            ["h"] = "close_node",
-            ["l"] = "open",
-          },
-        },
-      })
-      vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", { noremap = true, silent = true })
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local telescope = require("telescope")
-      telescope.setup()
-      vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true })
-      vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true })
-      vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { noremap = true })
-      vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-    },
-  },
-  { "L3MON4D3/LuaSnip" },
-  {
-    "stevearc/conform.nvim",
-    opts = {},
-  },
-  {
-    "mfussenegger/nvim-lint",
-    config = function()
-      require("lint").linters_by_ft = {
-        python = { "flake8" },
-        javascript = { "eslint" },
-        typescript = { "eslint" },
-        -- add more filetypes/linters as needed
-      }
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        callback = function()
-          require("lint").try_lint()
+        config = function()
+            require("neo-tree").setup({
+                filesystem = {
+                    follow_current_file = {
+                        enabled = true,
+                    },
+                    hijack_netrw_behavior = "open_default",
+                    filtered_items = { visible = false },
+                },
+                window = {
+                    mappings = {
+                        ["<space>"] = "none",
+                        ["o"] = "open",
+                        ["<cr>"] = "open",
+                        ["h"] = "close_node",
+                        ["l"] = "open",
+                    },
+                },
+            })
+            vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", { noremap = true, silent = true })
         end,
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "python", "typescript", "json" },
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup()
+            vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true })
+            vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true })
+            vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { noremap = true })
+            vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
         },
-      })
-    end,
-  },
+    },
+    { "L3MON4D3/LuaSnip" },
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+    },
+    {
+        "mfussenegger/nvim-lint",
+        config = function()
+            require("lint").linters_by_ft = {
+                python = { "flake8" },
+                javascript = { "eslint" },
+                typescript = { "eslint" },
+                -- add more filetypes/linters as needed
+            }
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function()
+                    require("lint").try_lint()
+                end,
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "python", "typescript", "json" },
+                auto_install = true,
+                ignore_install = {},
+                sync_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                modules = {},
+            })
+        end,
+    },
 })
